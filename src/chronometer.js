09163f55ed50413c8f67b612464e5d1fa2b45214
Courtney Dunn <courtney.a.dunn@gmail.com>
2023-05-06 06:44:41 -0400

    updated chronometer, still have to work through iterations, currently on iteration 6

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,26 +1,40 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    const intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if(printTimeCallback){
+        printTimeCallback();
+      }
+    }, 1000);
+
   }
 
   getMinutes() {
     // ... your code goes here
+   return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value.toString().padStart(2, '0');
+
   }
 
   stop() {
     // ... your code goes here
+    Chromometer.clearInterval(this.intervalId);
   }
 
   reset() {
